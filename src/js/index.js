c99dc80aea57520df5faeb9b315fe62a65b0e23c
Nikitapil <nikitapiliavets@MacBook-Pro-Nikita.local>
2021-10-04 22:22:05 +0300

    Add cart functional

--- src/js/index.js
+++ src/js/index.js
@@ -27,6 +27,18 @@ if (targetelement.classList.contains("products__morebtn")) {
     let allProducts = document.querySelectorAll('.products__item')
     allProducts.forEach((item) => item.style.display = "block")
 }
+if (targetelement.classList.contains("product__cartbtn")) {
+    addToCart (targetelement)
+}
+if (targetelement.classList.contains("header__actions__bin")) {
+    if (cartlist.children.length) {
+    cartContainer.classList.toggle("activecart")
+    }
+}
+if (!targetelement.closest(".cart__container") && document.querySelector(".activecart") && !targetelement.classList.contains("cartitem__delete")) {
+    document.querySelector(".activecart").classList.remove("activecart")
+    console.log(targetelement);
+}
 }
 const mainSlider = document.querySelector(".main__slider__slider")
 mainSlider.addEventListener("mouseenter", function(){
@@ -35,7 +47,7 @@ mainSlider.addEventListener("mouseenter", function(){
 mainSlider.addEventListener("mouseleave", function(){
     swiper.autoplay.start()
 })
-
+//Главный слайдер
 const swiper = new Swiper('.swiper', {
     observer: true,
     effect: "coverflow",
@@ -64,7 +76,7 @@ const swiper = new Swiper('.swiper', {
         prevEl: ".main__slider__prevbtn"
     }
 });
-
+// подгрузка продуктов
 let productsContainer = document.querySelector('.products')
 async function getProducts () {
     const file = "../json/products.json"
@@ -77,6 +89,7 @@ async function getProducts () {
         let productsTemplate = products.map((item)=> {
             let productItem = document.createElement("article")
         productItem.classList.add("products__item")
+        productItem.id = item.id
         let labelsItems = []
         if (item.labels) {
              labelsItems = item.labels.map((item)=>{
@@ -87,7 +100,6 @@ async function getProducts () {
                 return lab.outerHTML
             })
         }
-        console.log(labelsItems.join(" "));
         productItem.innerHTML = `<div class="product__head">
         <div class="product__img"><img src=${item.image} alt=${item.title}></div>
         <div class="product__labels">
@@ -116,27 +128,88 @@ async function getProducts () {
     }
 }
 getProducts ()
-/* let test = []
-let products = document.querySelector('.products')
-async function takeJson () {
-    let test2 = await fetch('https://fakestoreapi.com/products').then(res=>res.json())
-     console.log(test2);
-     test.push(test2)
-     console.log(test2[0].title);
-     let product = document.createElement("div")
-     product.innerHTML = `<img src=${test2[0].image}>`
-     products.append(product)
+
+//отправка товара в корзину
+const cartBtn = document.querySelector('.header__actions__bin')
+const cartCounter = document.querySelector('.header__actions__bin span')
+const cartlist = document.querySelector('.cart__list')
+const cartContainer = document.querySelector(".cart")
+function addToCart (button) {
+    let product = button.closest(".products__item")
+    let productTitle = product.querySelector(".product__title")
+    let productImage = product.querySelector(".product__img")
+    let flyImage = productImage.cloneNode(true)
+     flyImage.classList.add("flyimage")
+    let flyImageWidth = productImage.offsetWidth
+    let flyImageHeight = productImage.offsetHeight
+    let flyImageTop = productImage.getBoundingClientRect().top
+    let flyImageLeft = productImage.getBoundingClientRect().left
+    flyImage.style.cssText = `
+    left: ${flyImageLeft}px;
+    top: ${flyImageTop}px;
+    width: ${flyImageWidth}px;
+    height: ${flyImageHeight}px
+    `
+    document.body.append(flyImage)
+
+    const cartLeft = cartBtn.getBoundingClientRect().left
+    const cartTop = cartBtn.getBoundingClientRect().top
+    flyImage.style.cssText = `
+    left: ${cartLeft}px;
+    top: ${cartTop}px;
+    width: 0px;
+    height: 0px
+    `
+    let cartItemCheck = cartlist.querySelector(`[data-id="${product.id}"]`)
+    if (!cartItemCheck) {
+    let cartItem = document.createElement("li")
+    cartItem.setAttribute("data-id", product.id)
+    cartItem.classList.add("cartitem")
+    cartItem.innerHTML = `
+    <div class="cartitem__img">${flyImage.innerHTML}</div>
+<div class="cartitem__content">
+<p class="cartitem__title">${productTitle.textContent}</p>
+<p class="cartitem__quantity">Quantity:   <span class="cartitem__quantity__count">1</span></p>
+<button class="cartitem__delete">Delete</button>
+</div>
+    `
+    cartlist.append(cartItem)
+    }
+    else {
+       let cartItemcounter = cartItemCheck.querySelector(".cartitem__quantity__count")
+       cartItemcounter.textContent++
+    }
+    cartCounterEdit ()
+    flyImage.addEventListener("transitionend", function () {
+        cartCounter.style.display = "block";
+    })
 }
-takeJson() */
-/* async function takeTest () {
-await takeJson ()
-console.log(test[0][0].title);
+//Удаление из корзины
+cartContainer.addEventListener("click", function(event) {
+    let targetEl = event.target
+    if (targetEl.classList.contains("cartitem__delete")) {
+        let cartItem =  targetEl.closest(".cartitem")
+        let quantity = cartItem.querySelector(".cartitem__quantity__count")
+        quantity.textContent--
+        if (quantity.textContent == 0) {
+            cartItem.remove()
+        }
+        cartCounterEdit ()
+    }
+})
+function cartCounterEdit () {
+    let counters = Array.from(cartlist.querySelectorAll(".cartitem__quantity__count")).map((item)=>item.textContent)
+    if (counters.length) {
+    let cartsum = Array.from(counters).reduce((prev, item)=> Number(prev)+Number(item))
+    cartCounter.textContent = cartsum
+    }
+    else {
+        cartCounter.style.display = "none"
+        if (cartContainer.classList.contains("activecart")) {
+            cartContainer.classList.remove("activecart")
+        }
+    }
 }
-takeTest () */
-/* category: "men's clothing"
-description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
-id: 1
-image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-price: 109.95
-rating: {rate: 3.9, count: 120}
-title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 */
+
+
+
